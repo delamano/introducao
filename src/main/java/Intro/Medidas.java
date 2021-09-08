216f1dc065267b96a0ca119446d4f8d2c26d7ac7
delamano <delamano@live.com>
2021-09-08 17:26:08 -0300

    dia 2 de intro

---
+++ src/main/java/Intro/Medidas.java
@@ -0,0 +1,22 @@
+//Pacotes
+package Intro;
+
+//Referências as bibliotecas
+
+//classe
+public class Medidas {
+    //Atributos (Características)
+    //Métodos e Funções (verbos - o que a coisa sabe fazer)
+    //Método (faz e não retorna) Função (faz e retorna)
+
+    public static void main(String[] arg){
+        System.out.println("Meu primeiro código em Java");
+
+    int largura = 4;
+    int comprimento = 3;
+        System.out.println("a largura de " + largura + " m² "
+                            + "e o comprimento de " + comprimento +" m² "
+                            + "formam uma área de " + largura * comprimento + " m²");
+    }
+
+}
