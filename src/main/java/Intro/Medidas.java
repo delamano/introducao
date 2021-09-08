0c02d3f6a0198cb88a50dfd238623641456eed8c
delamano <delamano@live.com>
2021-09-08 20:26:23 -0300

    Usando Switch Case

--- src/main/java/Intro/Medidas.java
+++ src/main/java/Intro/Medidas.java
@@ -3,6 +3,8 @@ package Intro;
 
 //Referências as bibliotecas
 
+import java.util.Scanner;
+
 //classe
 public class Medidas {
     //Atributos (Características)
@@ -10,13 +12,26 @@ public class Medidas {
     //Método (faz e não retorna) Função (faz e retorna)
 
     public static void main(String[] arg){
-        System.out.println("Meu primeiro código em Java");
+        System.out.println("Cálculo de áreas");
 
     int largura = 4;
     int comprimento = 3;
         System.out.println("a largura de " + largura + " m² "
                             + "e o comprimento de " + comprimento +" m² "
                             + "formam uma área de " + largura * comprimento + " m²");
+
+        Scanner sc = new Scanner(System.in);
+
+        String opcao = sc.next();
+        switch (opcao){
+            case "a":
+                System.out.println("Opção A");
+                break;
+            case "b":
+                System.out.println("Opção B");
+                break;
+        }
+
     }
 
 }
